9cab9a9d7e414b46c9c45ce49c8c9e4dd3892e3e
Joshua Mukasa <joshmukasa@gmail.com>
2019-07-14 20:39:53 -0400

    Security updates

--- src/demo/App.js
+++ src/demo/App.js
@@ -2,18 +2,29 @@ import React from 'react';
 import './App.css'
 import { Button, Input } from '../lib';
 
-const App = () => (
-  <div>
-    <section>
-      <Button type="primary">Primary</Button >
-      <Button type="danger">Danger</Button >
-      <Button type="text">Text Button</Button >
-      <Button disabled type="primary">Disabled</Button >
-    </section>
-    <section>
-      <Input>Hi</Input>
-    </section>
-  </div>
-);
+class App extends React.Component{
+  state ={
+    button: false
+  }
+  buttonToggle = () =>{
+    this.setState({button: !this.state.button})
+  }
+  render(){
+    return(
+      <div>
+        <section>
+          <Button onClick={this.buttonToggle} type="primary">Toggle</Button >
+          <Button disabled={this.state.button} type="primary">Primary</Button >
+          <Button disabled={this.state.button} type="danger">Danger</Button >
+          <Button disabled={this.state.button} type="text">Text Button</Button >
+          <Button disabled type="primary">Disabled</Button >
+        </section>
+        <section>
+          <Input>Hi</Input>
+        </section>
+      </div>
+    );
+  }
+}
 
 export default App;
